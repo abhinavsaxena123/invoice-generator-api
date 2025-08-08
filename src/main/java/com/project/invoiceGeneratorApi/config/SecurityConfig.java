f3d93739a49c7ebb6b216203a2c78200e6e36d79
abhinavsaxena123 <saxenaabhinav113@gmail.com>
2025-08-08 23:46:42 +0530

    updated security

--- src/main/java/com/project/invoiceGeneratorApi/config/SecurityConfig.java
+++ src/main/java/com/project/invoiceGeneratorApi/config/SecurityConfig.java
@@ -52,7 +52,11 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         // Define your CORS rules
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",  // For local development
+                "https://the-invoicery.netlify.app"   // For our deployed site
+                )
+        );
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
